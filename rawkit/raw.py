d36600fadd3c8e87adc6186bb25b79389a0836ea
Cameron Paul <cpaul37@gmail.com>
2015-05-17 01:39:58 -0500

    Added basic development capability

--- rawkit/raw.py
+++ rawkit/raw.py
@@ -1,2 +1,22 @@
+from rawkit.libraw import libraw
+
+
 class Raw(object):
-    pass
+
+    def __init__(self, filename=None):
+        self.data = libraw.libraw_init(0)
+        libraw.libraw_open_file(self.data, bytes(filename, 'utf-8'))
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        libraw.libraw_close(self.data)
+
+    def process(self, options=None):
+        libraw.libraw_unpack(self.data)
+        libraw.libraw_dcraw_process(self.data)
+
+    def save(self, filename=None):
+        libraw.libraw_dcraw_ppm_tiff_writer(
+            self.data, bytes(filename, 'utf-8'))
