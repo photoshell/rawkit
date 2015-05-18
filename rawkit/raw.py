5cb8d2a4187d867111b32491df6e53983f124d73
Sam Whited <sam@samwhited.com>
2015-05-18 18:07:26 -0500

    Add close method to Raw class
    Fixes #10

--- rawkit/raw.py
+++ rawkit/raw.py
@@ -11,9 +11,19 @@ class Raw(object):
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
+        """Clean up after ourselves when leaving the context manager."""
+        self.close()
+
+    def close(self):
+        """Free the underlying raw representation."""
         libraw.libraw_close(self.data)
 
     def process(self, options=None):
+        """
+        Unpack and process the raw data into something more usable.
+
+        """
+
         libraw.libraw_unpack(self.data)
         libraw.libraw_dcraw_process(self.data)
 
