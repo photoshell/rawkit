731f02ddc4f943e48376860cbe733d350ec00a9b
Cameron Paul <cpaul37@gmail.com>
2015-05-19 23:43:06 -0500

    Added more `pragma: no cover` for travis hack

--- rawkit/libraw.py
+++ rawkit/libraw.py
@@ -243,10 +243,10 @@ class libraw_processed_image_t(Structure):
 # TODO: This is necessary because Travis CI is still using Ubuntu 12.04
 try:
     # TODO: This will do bad things if the API version isn't 10
-    libraw = cdll.LoadLibrary(util.find_library('raw'))
-    libraw.libraw_init.restype = POINTER(libraw_data_t)
-    libraw.libraw_dcraw_make_mem_image.restype = POINTER(
-        libraw_processed_image_t
-    )
-except:  # pragma: no cover
-    libraw = cdll.LoadLibrary('')  # pragma: no cover
+    libraw = cdll.LoadLibrary(util.find_library('raw'))    # pragma: no cover
+    libraw.libraw_init.restype = POINTER(libraw_data_t)    # pragma: no cover
+    libraw.libraw_dcraw_make_mem_image.restype = POINTER(  # pragme: no cover
+        libraw_processed_image_t                           # pragma: no cover
+    )                                                      # pragma: no cover
+except:                                                    # pragma: no cover
+    libraw = cdll.LoadLibrary('')                          # pragma: no cover
