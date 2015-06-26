ec0a38e390a7d7445e17eff2aaa2685ebf8bace8
Sam Whited <sam@samwhited.com>
2015-06-25 20:00:04 -0500

    Update rawkit module docs
    Remove faulty pre-commit hook

--- rawkit/__init__.py
+++ rawkit/__init__.py
@@ -1,3 +1,23 @@
+""":mod:`rawkit` --- Raw photo manipulation
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+The :mod:`rawkit` module contains high-level APIs for manipulating raw photos
+using the low-level :mod:`libraw` module (which in turn uses the even
+lower-level LibRaw C library).
+
+Eg. quickly processing a raw Canon CR2 file without using the camera white
+balance and saving it as a PPM image might look like this:
+
+.. sourcecode:: python
+
+    from rawkit.raw import Raw
+    from rawkit.options import WhiteBalance
+
+    with Raw(filename='some/raw/image.CR2') as raw:
+      raw.options.white_balance = WhiteBalance(camera=False, auto=True)
+      raw.save(filename='some/destination/image.ppm')
+"""
+
 VERSION = '0.1.0'
 """
 The current version of the `rawkit` package.
