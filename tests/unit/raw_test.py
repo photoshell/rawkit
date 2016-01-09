6166e462c4b3034672ce90ae44e9c8c01f70a77f
Cameron Paul <cpaul37@gmail.com>
2016-01-09 01:18:10 -0600

    Added tests for bayer_data

--- tests/unit/raw_test.py
+++ tests/unit/raw_test.py
@@ -1,6 +1,8 @@
 import mock
 import os
 import pytest
+import warnings
+
 from rawkit.errors import InvalidFileType, NoFileSpecified
 from rawkit.metadata import Metadata
 from rawkit.raw import Raw, DarkFrame
@@ -31,6 +33,18 @@ def dark_frame(input_file):
         with DarkFrame(filename=input_file) as raw_obj:
             yield raw_obj
         raw_obj.libraw.libraw_close.assert_called_once_with(raw_obj.data)
+
+
+@pytest.yield_fixture
+def mock_warning():
+    with mock.patch.object(warnings, 'warn') as mock_warning:
+        yield mock_warning
+
+
+@pytest.yield_fixture
+def mock_ctypes():
+    with mock.patch('rawkit.raw.ctypes') as mock_ctypes:
+        yield mock_ctypes
 
 
 def test_create(raw, input_file):
@@ -160,11 +174,9 @@ def test_save_thumb_no_filename(raw):
         raw.save_thumb()
 
 
-def test_to_buffer(raw):
-    # Quick hack because to_buffer does some ctypes acrobatics
-    with mock.patch('rawkit.raw.ctypes'):
-        with mock.patch('rawkit.raw.raise_if_error'):
-            raw.to_buffer()
+def test_to_buffer(raw, mock_ctypes):
+    with mock.patch('rawkit.raw.raise_if_error'):
+        raw.to_buffer()
 
     raw.libraw.libraw_dcraw_make_mem_image.assert_called_once_with(
         raw.data,
@@ -176,11 +188,9 @@ def test_to_buffer(raw):
     )
 
 
-def test_thumbnail_to_buffer(raw):
-    # Quick hack because thumbnail_to_buffer does some ctypes acrobatics
-    with mock.patch('rawkit.raw.ctypes'):
-        with mock.patch('rawkit.raw.raise_if_error'):
-            raw.thumbnail_to_buffer()
+def test_thumbnail_to_buffer(raw, mock_ctypes):
+    with mock.patch('rawkit.raw.raise_if_error'):
+        raw.thumbnail_to_buffer()
 
     raw.libraw.libraw_dcraw_make_mem_thumb.assert_called_once_with(
         raw.data,
@@ -195,3 +205,57 @@ def test_thumbnail_to_buffer(raw):
 def test_metadata(raw):
     metadata = raw.metadata
     assert type(metadata) is Metadata
+
+
+def test_get_bayer_data(raw, mock_ctypes):
+    raw.data.contents.sizes.pixel_aspect = 1
+    raw.data.contents.sizes.flip = 0
+
+    result = raw.bayer_data()
+
+    assert result is not None
+
+
+def test_get_bayer_data_bad_aspect(raw, mock_ctypes, mock_warning):
+    raw.data.contents.sizes.pixel_aspect = 2
+    raw.data.contents.sizes.flip = 0
+
+    raw.bayer_data()
+
+    mock_warning.assert_called_with(
+        'The pixel aspect is not unity, it is: 2'
+    )
+
+
+def test_get_bayer_data_flip(raw, mock_ctypes, mock_warning):
+    raw.data.contents.sizes.pixel_aspect = 1
+    raw.data.contents.sizes.flip = 1
+
+    raw.bayer_data()
+
+    mock_warning.assert_called_with(
+        'The image is flipped.'
+    )
+
+
+def test_bayer_data_non_bayer_image(raw, mock_ctypes):
+    raw.data.contents.sizes.pixel_aspect = 1
+    raw.data.contents.sizes.flip = 0
+
+    # The falsiness of this value is what is tested to see if bayer data
+    # exists, so we set this to False even though it's supposed to be a
+    # pointer.
+    raw.data.contents.rawdata.raw_image = False
+
+    result = raw.bayer_data()
+
+    assert result is None
+
+
+def test_bayer_data_with_margin(raw, mock_ctypes):
+    raw.data.contents.sizes.pixel_aspect = 1
+    raw.data.contents.sizes.flip = 0
+
+    result = raw.bayer_data(include_margin=True)
+
+    assert result is not None
