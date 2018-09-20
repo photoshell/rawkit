90a9285c215974c24bcf9bf23891c1b9f5a5f4c4
Sam Whited <sam@samwhited.com>
2018-09-20 16:40:38 -0500

    Support full rawkit to exif orientation mapping
    Fixes #120

--- rawkit/metadata.py
+++ rawkit/metadata.py
@@ -5,15 +5,6 @@
 from collections import namedtuple
 
 
-Orientation = namedtuple('Orientation', [
-    'landscape',
-    'portrait',
-])(0, 1)
-"""
-Represents the orientation of an image. Either `landscape` or `portrait`.
-"""
-
-
 Metadata = namedtuple('Metadata', [
     'aperture',
     'timestamp',
@@ -29,4 +20,23 @@ Metadata = namedtuple('Metadata', [
 ])
 """
 Common metadata for a photo.
+
+Orientation matches the values from the EXIF 2.3 specification:
+
+1 - The 0th row is at the visual top of the image, and the 0th
+    column is the visual left-hand side.
+2 - The 0th row is at the visual top of the image, and the 0th
+    column is the visual right-hand side.
+3 - The 0th row is at the visual bottom of the image, and the
+    0th column is the visual right-hand side.
+4 - The 0th row is at the visual bottom of the image, and the
+    0th column is the visual left-hand side.
+5 - The 0th row is the visual left-hand side of the image, and
+    the 0th column is the visual top.
+6 - The 0th row is the visual right-hand side of the image,
+    and the 0th column is the visual top.
+7 - The 0th row is the visual right-hand side of the image,
+    and the 0th column is the visual bottom.
+8 - The 0th row is the visual left-hand side of the image, and
+    the 0th column is the visual bottom.
 """
--- rawkit/orientation.py
+++ rawkit/orientation.py
@@ -1,8 +1,34 @@
+# EXIF 2.3:
+#
+# 1 - The 0th row is at the visual top of the image, and the 0th
+#     column is the visual left-hand side.
+# 2 - The 0th row is at the visual top of the image, and the 0th
+#     column is the visual right-hand side.
+# (mirror horizontal)
+# 3 - The 0th row is at the visual bottom of the image, and the
+#     0th column is the visual right-hand side.
+# 4 - The 0th row is at the visual bottom of the image, and the
+#     0th column is the visual left-hand side.
+# (mirrored vertical)
+# 5 - The 0th row is the visual left-hand side of the image, and
+#     the 0th column is the visual top.
+# (
+# 6 - The 0th row is the visual right-hand side of the image,
+#     and the 0th column is the visual top.
+# 7 - The 0th row is the visual right-hand side of the image,
+#     and the 0th column is the visual bottom.
+# 8 - The 0th row is the visual left-hand side of the image, and
+#     the 0th column is the visual bottom.
 exif_orientation_map = {
-    0: 1,
-    6: 6,
-    3: 3,
-    5: 8,
+    # libraw: exif
+    0: 1,  # No rotation
+    1: 2,  # mirror horizontal
+    2: 4,  # mirror vertical
+    3: 3,  # 180-deg
+    4: 5,  # 90ccw, mirror vertical
+    5: 8,  # 90ccw
+    6: 6,  # 90cw
+    7: 7,  # 90cw, mirror vertical
 }
 
 
